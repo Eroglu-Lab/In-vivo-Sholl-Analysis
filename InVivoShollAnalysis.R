017672009588d26c473df226e5b881edff6cea61
kristina-sakers <65187156+kristina-sakers@users.noreply.github.com>
2023-03-31 11:18:39 -0400

    Update InVivoShollAnalysis.R

--- InVivoShollAnalysis.R
+++ InVivoShollAnalysis.R
@@ -4,7 +4,6 @@ library(ggplot2)
 library(reshape)
 library(car)
 library(agricolae)
-library(xlsx)
 library(lme4)
 library(nlme)
 require(multcomp)
@@ -12,40 +11,29 @@ library(ggplot2)
 
 
 #Read in data
-d <- read.xlsx('', 1) #enter path of file
-d$Image <- make.unique(d$Image)
-
-
-d2 <- melt(d, id.vars=c('Image', 'Condition', 'Age'))
-d2$Condition <- factor(d2$Condition)
-d2$variable <- gsub('X', '', d2$variable)
-d2$variable <- as.numeric(d2$variable)
-
-names(d2) <- c('Image', 'Condition', 'Age', 'Radius', 'Intersections')
-d2$Age <- factor(d2$Age, levels = c('P7', 'P14', 'P21', 'P63'))
+d <- read.csv('sholl_InputData.csv') #enter path of file
 
 #loess smoothening
-ggplot(d2, aes(x=Radius, y=Intersections, group=Condition, fill=Condition)) + 
-  stat_smooth(col='black', method='loess', span=0.5) + ylab('# Intersections') + 
-  xlab ('Distance from Soma (um)') + theme(axis.text=element_text(size=12), axis.title=element_text(size=14,face="bold")) +
-  facet_grid(~Age)
-
+ggplot(d, aes(x=Radius, y=Intersections, group=condition, fill=condition)) + 
+  stat_smooth(col='black', method='loess') + ylab('# Intersections') + 
+  xlab ('Distance from Soma (um)') + theme(axis.text=element_text(size=12), axis.title=element_text(size=14,face="bold"))
 
 #error bars mean +/- SEM
-ggplot(d2, aes(x=Radius, y=Intersections, color=Condition, group=Condition)) + 
+ggplot(d, aes(x=Radius, y=Intersections, color=condition, group=condition)) + 
   stat_summary(geom='line', fun=mean) +
-  stat_summary(fun.data=mean_se, geom='pointrange') + ylab('# Intersections') + theme_minimal()  +
-  facet_grid(~Age)
+  stat_summary(fun.data=mean_se, geom='pointrange') + ylab('# Intersections') + theme_minimal()
 
 
 #####################################################################
 ############               STATISTICS                     ###########
 #####################################################################
 
+d$condition <- as.factor(d$condition)
+
 cond_test_corAR <- nlme::lme(
-  Intersections ~  1 + Condition,
-  data = d2,
-  random = ~ 1 | Image / Radius ,
+  Intersections ~  1 + condition,
+  data = d,
+  random = ~ 1 | Image.Name / Radius ,
   correlation = corAR1()
 )
 
@@ -54,11 +42,14 @@ summary(cond_test_corAR)
 AOV <- anova(cond_test_corAR)
 AOV
 
-tukeys <- summary(glht(cond_test_corAR, linfct=mcp(Condition="Tukey")))
+tukeys <- summary(glht(cond_test_corAR, linfct=mcp(condition="Tukey")))
 tukeysPH <- data.frame(as.character(row.names(tukeys$linfct)), tukeys$test$pvalues)
+
+write.csv(tukeysPH, 'tukeysPostHoc_pvalues.csv', quote=F, row.names=F)
+write.csv(AOV, 'ANOVA_output.csv', quote=F)
 
 
 #determine N
-for (i in unique(d2$Condition)){
-  print(paste(i, nrow(d2[d2$Condition == i & d2$Radius == 5, ]), sep='='))
+for (i in unique(d$condition)){
+  print(paste(i, nrow(d[d$condition == i & d$Radius == 5, ]), sep='='))
 }
